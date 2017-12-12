9e85df1635e55be7a08397ac72ebd2c0c9274f1b
Przemek Marciniak <przemek.marciniak@racebets.com>
2017-12-12 17:50:06 +0100

    day 10 part 2 temp

--- day10/10-2.js
+++ day10/10-2.js
@@ -1,47 +1,44 @@
 'use strict';
-module.exports = data => {
-    let position = 0,
-        skipSize = 0,
-        map = Array(256).fill('').map((x, i) => i),
-        rounds = 0;
 
-        const pref = [17, 31, 73, 47, 23];
+const day10 = module.exports = data => {
 
-    let lengths = data.trim().split('').map(x=>{
-        return x = x.charCodeAt(0);
-    });
+    function getHash(list, ins, i, skip) {
+        const l = list.length;
 
-    lengths = [...lengths, ...pref];
+        ins.forEach(v => {
+            [...Array(v).keys()]
+                .map((o, k) => list[(k + i) % l])
+                .reverse()
+                .forEach((val, k) => (list[(k + i) % l] = val));
 
-    console.log(lengths);
+            i += v + skip;
+            skip++;
+        });
 
-    while (rounds++ < 64) {
-        console.log(rounds);
+        return [list, i, skip];
     }
 
-    // while (typeof lengths[skipSize] !== 'undefined') {
-    //     const thisLength = lengths[skipSize],
-    //         startIdx = position,
-    //         endIdx = (position + thisLength) % map.length;
-    //
-    //     let secondSlice, lenght2, temp;
-    //
-    //     if (startIdx > endIdx && thisLength !== 0) {
-    //         secondSlice = map.slice(endIdx, startIdx);
-    //         lenght2 = map.slice(startIdx).length;
-    //         temp = [...map.slice(startIdx), ...map.slice(0, endIdx)].reverse();
-    //         map = [...temp.slice(lenght2), ...secondSlice, ...temp.slice(0, lenght2)];
-    //     } else if (startIdx === endIdx && thisLength !== 0) {
-    //         lenght2 = map.slice(startIdx).length;
-    //         temp = [...map.slice(startIdx), ...map.slice(0, endIdx)].reverse();
-    //         map = [...temp.slice(lenght2).reverse(), ...temp.slice(0, lenght2)];
-    //     } else if (thisLength !== 0) {
-    //         map = [...map.slice(0, startIdx), ...map.slice(startIdx, endIdx).reverse(), ...map.slice(endIdx)];
-    //     }
-    //
-    //     position = (position + thisLength + skipSize) % map.length;
-    //     skipSize++;
-    //
-    // }
-    // return map[0] * map[1];
+    function init(input) {
+        let skip = 0,
+            i = 0,
+            list = [...Array(256).keys()];
+
+        input = input.trim().split('').map(v => v.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
+
+        for (let k = 0; k < 64; ++k) {
+            [list, i, skip] = getHash(list, input, i, skip);
+        }
+        const dense = [];
+
+        let j = 0;
+
+        while (j < list.length) {
+            dense.push(list.slice(j, j + 16).reduce((xor, cur) => xor ^ cur));
+            j += 16;
+        }
+
+        return dense.map(n => (`00${n.toString(16)}`).substr(-2)).join('');
+    }
+
+    return init(data);
 };
